9e29cfdbf3e62e537e40bd049d59c96ce8269fdf
qi shijun <2100630861@qq.com>
2024-09-20 19:38:26 +0800

    标签页名称修改

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin());
+
+    // 设置网页标签页标题
+    config.plugin("html").tap((args) => {
+      args[0].title = "acoj";
+      return args;
+    });
   },
 });
